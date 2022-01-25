33b8a28fe222a36fe3c14b53fd5fdce2b44d85dd
albafika18 <jhon.rojas@nextpro.pe>
2022-01-25 17:33:40 -0500

    prueba con diccionario ya que estos aún no están creados.

--- custom_sale/models/sale.py
+++ custom_sale/models/sale.py
@@ -66,7 +66,7 @@ class SaleOrder(models.Model):
         products = self.order_line.mapped('product_id.id')
         unique_products = set(products)
 
-        _logger.info("\n\n\nMOSTRAR DATOS %s\n\n\n", [unique_products, products])
+        _logger.info("\n\n\nMOSTRAR DATOS %s\n\n\n", [unique_products, self['order_line']])
         _logger.info("\n\n\nMOSTRAR DATOS %s\n\n\n", [len(unique_products), len(self.order_line)])
 
         if len(unique_products) != len(self.order_line):
