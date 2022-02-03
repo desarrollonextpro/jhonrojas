27dad57dd596ddea255731297721efa035bbdc79
albafika18 <jhon.rojas@nextpro.pe>
2022-02-02 19:10:52 -0500

    prueba de herencia en report

--- custom_sale/__manifest__.py
+++ custom_sale/__manifest__.py
@@ -5,11 +5,12 @@
     'author': 'Jhon Jairo Rojas Ortiz',
     'license': 'LGPL-3',
     'depends': [
-        'sale', 'base_automation', 'project'
+        'sale', 'base_automation', 'project', 'purchase'
     ],
     'data': [
         'security/ir_model_access.xml',
         'data/base_automation.xml',
+        'report/purchase_order_template.xml',
         'views/transfer_type.xml',
         'views/sale.xml'
     ],
