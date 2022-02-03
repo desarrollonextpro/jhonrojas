f826c623b164f10d609b6e6d05e444f3180345ae
albafika18 <jhon.rojas@nextpro.pe>
2022-02-02 19:38:02 -0500

    test de custom_website_sale (pruebas unitarias)

---
+++ custom_website_sale/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Custom website sale',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': 'Jhon Jairo Rojas Ortiz',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'website_sale', 'purchase'
+    ],
+    'data': [
+        'report/purchase_order_template.xml',
+        'views/purchase_order.xml'
+    ],
+    'auto_install': False,
+    'application': False,
+}
