89d4ae5ade89e37d14f508c6771a18b09cf7861b
albafika18 <jhon.rojas@nextpro.pe>
2022-02-08 17:09:54 -0500

    .

--- loan/models/loan.py
+++ loan/models/loan.py
@@ -36,5 +36,5 @@ class Loan(models.Model):
 
     @api.model
     def create(self, vals):
-        vals['name'] = f"Prestamo de {self.env['res.partner'].browse(vals.get('partner_id')).name or ''} en la fecha  {str(self.date)}"
+        vals['name'] = f"Prestamo de {self.env['res.partner'].browse(vals.get('partner_id')).name or ''} en la fecha  {self.date.strftime('%d/%m/%Y')}"
         return super(Loan, self).create(vals)
